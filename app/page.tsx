b7d40e83dc2f0e6960aed8549b330457ccccbd71
Davide Vignozzi <davide.vignozzi@gmail.com>
2024-01-11 12:15:10 +0100

    :lipstick: Add shadcnUI

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <h1>Projects</h1>
       <Link href="/projects/animation1">Animation 1</Link>
     </main>
   );
