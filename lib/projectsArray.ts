937bb6dd2aaed24d99dab358626f6dac95c49d8c
Davide Vignozzi <davide.vignozzi@gmail.com>
2024-01-11 16:48:24 +0100

    :technologist: Projects array improvements

--- lib/projectsArray.ts
+++ lib/projectsArray.ts
@@ -1,9 +1,16 @@
-export const projectsArray = [
-  {
-    name: 'trial',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus quo ullam qui illum nisi optio repellat earum ad commodi deserunt magni molestias laudantium eaque sint quos voluptatum accusamus, error doloremque!',
-    codeLink: '/',
-    link: '/projects/trial'
-  }
-];
+interface Project {
+  name: string;
+  description: string;
+  codeLink: string;
+  link: string;
+}
+
+const project1: Project = {
+  name: 'trial',
+  description:
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus quo ullam qui illum nisi optio repellat earum ad commodi deserunt magni molestias laudantium eaque sint quos voluptatum accusamus, error doloremque!',
+  codeLink: '/',
+  link: '/projects/trial'
+};
+
+export const projectsArray: Project[] = [project1];
